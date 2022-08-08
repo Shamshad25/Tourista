45b4bd8b05a1bba8e81bb4a65c522f1f2c3ae5ac
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2022-08-08 10:59:35 +0530

    fixed bug in List

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -9,10 +9,10 @@ import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   const location = useLocation();
-  const [destination, setDestination] = useState(location.state.destination);
+  const [destination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
-  const [options, setOptions] = useState(location.state.options);
+  const [options] = useState(location.state.options);
 
   return (
     <div>
